b4c322007f06c0a3965a938a89114acab0721548
Saswat Samal <39031660+saswatsamal@users.noreply.github.com>
2020-11-19 13:33:10 +0530

    Add files via upload

--- Numbers/floatingpoint.java
+++ Numbers/floatingpoint.java
@@ -1,19 +1,23 @@
 public class floatingpoint {
     public static void main(String[] args) {
-        // You want to know if a floating-point computation generated a sensible result.
+        // WAP for floating-point computation to compare 
+        // the result with POSITIVE_INFINITY, NEGATIVE_INFINITY,
+        // and NaN.
 
         double d = 123;
+        double f = -123;
         double e = 0;
 
         if (d/e == Double.POSITIVE_INFINITY)
             System.out.println("POSITIVE_INFINITY");
 
+        if (f/e == Double.NEGATIVE_INFINITY)
+            System.out.println("NEGATIVE_INFINITY");
+
         double s = Math.sqrt(-1);
 
         if (s == Double.NaN);
-            System.out.println("Comparison with NaN incorrectly returns true");
+            System.out.println("NaN");
 
-        if (Double.isNaN(s))
-            System.out.println("Double.isNaN() correctly returns true");
     }
 }
